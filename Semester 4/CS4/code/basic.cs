a1653c99b80bb2f8f4e941c12f2e8eb6ce034298
ahmad.abuzainab@outlook.com <fridge@Barrad>
2023-02-28 14:13:37 +0200

    CS time uwu

---
+++ Semester 4/CS4/code/basic.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace name_of_program_here {
+class Program {
+    static void Main(string[] args) {
+        /*
+         * Your code goes here
+         * */
+    }
+}
+}
---
+++ Semester 4/CS4/code/similarities.cs
@@ -0,0 +1,36 @@
+/* Variable assignment */
+int x = 5, i = 0;
+float y = 4.5;
+double z = 3.1415;
+char c = 'A';
+
+/* Basic Operations */
+x + y;
+x * y;
+x++;
+etc...
+
+/* Conditionals */
+if (x == y) {
+    /*...*/
+} else if (x < y) {
+    /*..*/
+} else {
+    /*...*/
+}
+
+/* Ternary Operator */
+int w = (x < y)? y : x;
+
+/* Loops */
+while (i < 6) {
+    /*..*/
+}
+
+do {
+    /*...*/
+} while (i < 6);
+
+for (int i = 0; i < 6; i++) {
+    /*...*/
+}
